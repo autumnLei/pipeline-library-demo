a34d24b93a29f876a87b704379873120d5301d42
huanglei <lei38.huang@tcl.com>
2021-07-07 14:22:54 +0800

    feat:update 01

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,5 +2,19 @@
 
 def call(String name = 'human') {
   echo "Hello, ${name}."
+  parallel codeScan: {
+    sh "echo this is codeScan"
+  }, apkScan: {
+    sh "echo this is apkScan"
+    sh "touch a"
+  }, failFast: true
+
+  archiveArtifacts artifacts: 'a', fingerprint: true, onlyIfSuccessful: true, allowEmptyArchive: true
+
+  try {
+    sh "exit 1"
+  } catch (exc) {
+    error "${exc}"
+  }
 }
 
