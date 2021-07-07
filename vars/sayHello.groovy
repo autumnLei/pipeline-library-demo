c554e0b136aaf89c987e4dc24204b47ab9a45504
huanglei <lei38.huang@tcl.com>
2021-07-07 15:41:53 +0800

    feat:update 02

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,14 +2,20 @@
 
 def call(String name = 'human') {
   echo "Hello, ${name}."
+  echo "BUILD_NUMBER: ${BUILD_NUMBER}"
+  echo "env.BUILD_NUMBER: ${env.BUILD_NUMBER}"
+  echo "gitlabSourceBranch: ${gitlabSourceBranch}"
+  echo "env.gitlabSourceBranch: ${env.gitlabSourceBranch}"
+  echo "email: ${email}"
+  echo "env.email: ${env.email}"
   parallel codeScan: {
     sh "echo this is codeScan"
   }, apkScan: {
     sh "echo this is apkScan"
-    sh "touch a"
+    sh "touch a.txt"
   }, failFast: true
 
-  archiveArtifacts artifacts: 'a', fingerprint: true, onlyIfSuccessful: true, allowEmptyArchive: true
+  archiveArtifacts artifacts: 'a.txt', fingerprint: true, onlyIfSuccessful: false, allowEmptyArchive: true
 
   try {
     sh "exit 1"
